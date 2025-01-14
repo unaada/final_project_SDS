6bc90e9e137df5a98d26d66d86e6c70203c18fbe
unaada <una.adamovica@gmail.com>
2025-01-14 13:12:17 +0200

    Clean the R code

--- moose_home_range_definition.R
+++ moose_home_range_definition.R
@@ -1,24 +1,19 @@
-attach(moose_collars_gps)
-
 # libraries
-library(sf) #younger
-library(ctmm)
-library(dplyr)
-library(raster)
-library(scales)
-library(terra)
+library(sf) #spatial object
+library(ctmm) #home range estimation package
+library(dplyr) #formatting
 library(readxl)
 
 
-# --- full dataset DONT CHANGE ----
+# --- full dataset ----
 estonian <- '+proj=lcc +lat_0=57.5175539305556 +lon_0=24 +lat_1=59.3333333333333 +lat_2=58 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
 
-moose_subset_spat <- st_as_sf(moose_collars_gps, coords = c("Lon", "Lat"), crs = 4326)
-moose_subset_spat <- st_transform(moose_subset_spat, crs = 32635)
+moose_subset_spat <- st_as_sf(moose_collars_gps, coords = c("Lon", "Lat"), crs = 4326) #create spatial object from dataset to get coordinates
+moose_subset_spat <- st_transform(moose_subset_spat, crs = 32635) #use UTM zone 35 North
 
 coordinates <- st_coordinates(moose_subset_spat)
 
-moose_subset_spat$Easting <- coordinates[, "X"]
+moose_subset_spat$Easting <- coordinates[, "X"] #add the coordinates from spatial object as Easting and Northing
 moose_subset_spat$Northing <- coordinates[, "Y"]
 
 moose_df_simple <- data.frame(
@@ -28,53 +23,26 @@ moose_df_simple <- data.frame(
   UTM.zone = "35 +north",
   timestamp = as.POSIXct(moose_subset_spat$LMT, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Tallinn")
   
-)
-
+) #reformat so as.telemtry would recognize the needed columns
 
 moose_df <- na.omit(moose_df)
-tel <- as.telemetry(moose_df, drop = FALSE, crs = 32635, projection = estonian, timezone = "Europe/Tallinn")
-
-
-fits_subset <- ctmm.fit(tel$"39794")
-summary(tel)
-akde_subset <- akde(tel$"39794", fits_subset)
-summary(akde_subset)
-
-plot(akde_subset)
-head(akde_subset)
+tel <- as.telemetry(moose_df, drop = FALSE, crs = 32635, projection = estonian, timezone = "Europe/Tallinn") #convert to telemetry object for use in akde
 
 
 
-
-
-
-#--- autocorrelation exists INDIVIDUAL ----
+#--- autocorrelation testing INDIVIDUAL ----
 
 tel_individual <- tel$"39794"
-guess <- ctmm.guess(tel_individual, interactive = TRUE)
-model <- ctmm.select(tel_individual, guess)
+guess <- ctmm.guess(tel_individual, interactive = TRUE) #initial ctmm parameter guess
+model <- ctmm.select(tel_individual, guess) #fit movement models, select best via AICc 
 summary(model)
-
 
 vario <- variogram(tel_individual)
 plot(vario, CTMM = guess)
 
+akde_result <- akde(tel_individual, CTMM = model) #autocorrelated kernel density home-range estimate
 
-akde_result <- akde(tel_individual, CTMM = model)
-
-
-plot(akde_result, level = 0.95)
-plot(tel_individual)
-
-
-
-
-
-
-
-
-
-
+plot(akde_result, level = 0.95) 
 
 #--- no restriction moose  ----
 
@@ -88,13 +56,13 @@ individuals <- c("39793", "39790", "39787")
 
 for (id in individuals) {
   try({
-    # extract the individual's telemetry data
+    #individual telemetry data
     tel_individual <- tel[[id]]
     
     # initial model guess
     guess <- ctmm.guess(tel_individual, interactive = FALSE)
     
-    # fit the model
+    # fit the best model
     model <- ctmm.select(tel_individual, guess)
     
     # calculate the variogram
@@ -105,12 +73,12 @@ for (id in individuals) {
     variogram_plots[[id]] <- recordPlot()
     
     
-    # compute the AKDE
+    # calculate the AKDE
     akde_result <- akde(tel_individual, CTMM = model, grid = list(dr=30, align.to.origin = TRUE))
     
     # record and save the AKDE plot
     plot(akde_result, level = 0.95)
-    akde_plots[[id]] <- recordPlot()  # Save the plot in memory
+    akde_plots[[id]] <- recordPlot()
     
     # store the results
     results[[id]] <- list(model = model, akde = akde_result)
@@ -131,12 +99,6 @@ writeVector(results[["39793"]]$akde,"39793_grid.gpkg",filetype="GPKG",convex=FAL
 writeVector(results[["39787"]]$akde,"39787_50.gpkg",filetype="GPKG",convex=FALSE,level.UD=0.5)
 writeVector(results[["39790"]]$akde,"39790_50.gpkg",filetype="GPKG",convex=FALSE,level.UD=0.5)
 writeVector(results[["39793"]]$akde,"39793_50.gpkg",filetype="GPKG",convex=FALSE,level.UD=0.5)
-
-
-
-
-
-
 
 
 
@@ -170,15 +132,12 @@ plot(tel_39789, akde_result_39789, level = 0.95)
 
 
 
-
-
-
   #--- the bottom polygon as barrier ----
 results_restricted <- list()
 akde_plots_restricted <- list()
 variogram_plots_restricted <- list()
 
-# List of individuals to process
+#individuals to process
 individuals_to_process <- c("39785", "39788", "39791")
 
 # Loop through the selected moose individuals
@@ -196,14 +155,14 @@ for (id in individuals_to_process) {
     # variogram
     vario <- variogram(tel_individual)
     plot(vario, CTMM = guess)
-    variogram_plots_restricted[[id]] <- recordPlot()  # Save the plot in memory
+    variogram_plots_restricted[[id]] <- recordPlot()
     
     # AKDE within the restricted area (Tartu-Tallinn bottom part )
     akde_result <- akde(tel_individual, CTMM = model, grid = list(dr=30, align.to.origin = TRUE), SP = study_area_bottom)
     
     # record AKDE plot
     plot(akde_result, level = 0.95)
-    akde_plots_restricted[[id]] <- recordPlot()  # Save the plot in memory
+    akde_plots_restricted[[id]] <- recordPlot()
     
     # results
     results_restricted[[id]] <- list(model = model, akde = akde_result)
@@ -233,7 +192,7 @@ writeVector(results_restricted[["39791"]]$akde,"39791_50.gpkg",filetype="GPKG",c
 tel_39794 <- tel[["39794"]] 
 
 guess <- ctmm.guess(tel_39794, interactive = FALSE)
-models <- ctmm.select(tel_39794, guess,verbose = TRUE)
+models <- ctmm.select(tel_39794, guess,verbose = TRUE) #verbose=T to see all models
 
 results_39794 <- list()
 akde_plots_39794 <- list()
@@ -247,7 +206,7 @@ for (model_name in names(models)) {
     # compute the variogram and save the plot
     vario <- variogram(tel_39794)
     plot(vario, CTMM = model)
-    variogram_plots_39794[[model_name]] <- recordPlot()  # Save the plot
+    variogram_plots_39794[[model_name]] <- recordPlot()
     
     # compute AKDE for this model
     akde_result <- akde(tel_39794, CTMM = model, res = 10)
@@ -257,7 +216,7 @@ for (model_name in names(models)) {
     
     # plot the AKDE result and save
     plot(akde_result, level = 0.95, main = paste("AKDE for Model:", model_name))
-    akde_plots_39794[[model_name]] <- recordPlot()  # Save the plot
+    akde_plots_39794[[model_name]] <- recordPlot()
     
   }, silent = TRUE)
 }
@@ -269,14 +228,7 @@ replayPlot(variogram_plots_39794[["OU"]])
 lapply(results_39794, function(res) summary(res$akde))
 summary(models)
 
-
-writeVector(results_39794[["OUF"]]$akde, "39794_OUF", filetype = "ESRI Shapefile", convex = FALSE, level.UD = 0.95, level = 0.95)
-
-
-
-
-
-
+writeVector(results_39794[["OUF"]]$akde, "39794_OUF.gpkg", filetype = "GPKG", convex = FALSE, level.UD = 0.95, level = 0.95)
 
 
 
@@ -295,10 +247,6 @@ moose_collars_gps <- read_excel("D:/Users/amand/OneDrive - Tartu Ülikool/Master
 moose_collars_gps$LMT <- as.POSIXct(moose_subset_spat$LMT, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
 
-#--- attempting to align the grids
-
-
-
 #subset the dates when in winter habitat
 winter_data_grid <- subset(
   moose_collars_gps,
@@ -307,13 +255,13 @@ winter_data_grid <- subset(
        (LMT > "2019-03-12" & LMT < "2019-04-13"))
 )
 
-# Correct CRS and projection for winter
+# CRS and projection for winter
 winter_sf_grid <- st_transform(st_as_sf(winter_data_grid, coords = c("Lon", "Lat"), crs = 4326), crs = 32635) ## change of CRS
 winter_coordinates_grid <- st_coordinates(winter_sf_grid)
 winter_sf_grid$Easting <- winter_coordinates_grid[, "X"]
 winter_sf_grid$Northing <- winter_coordinates_grid[, "Y"]
 
-# Reformat in the way that as.telemetry accepts
+#format in the way that as.telemetry accepts
 winter_df_grid <- data.frame(
   id = as.factor(winter_sf_grid$CollarID),
   UTM.Easting = winter_sf_grid$Easting,
@@ -322,24 +270,23 @@ winter_df_grid <- data.frame(
   timestamp = as.POSIXct(winter_sf_grid$LMT, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Tallinn")
 )
 
-# Clean the data
 winter_df_grid <- na.omit(winter_df_grid)
 
-# Get telemetry
+
 winter_tel_grid <- as.telemetry(winter_df_grid, 
                                 drop = FALSE, 
                                 crs = 32635, 
                                 projection = estonian, 
                                 timezone = "Europe/Tallinn") # testing
 
-# Model selection and estimation
+#model selection and estimation
 guess_winter_grid <- ctmm.guess(winter_tel_grid[["39794"]], interactive = FALSE)
 model_winter_grid <- ctmm.select(winter_tel_grid[["39794"]], guess_winter_grid)
 akde_result_winter_grid <- akde(winter_tel_grid[["39794"]], 
                                 CTMM = model_winter_grid, 
                                 grid = list(dr = 30, align.to.origin = TRUE))
 
-# Export results
+#export probability distribution function raster results
 writeRaster(akde_result_winter_grid,
             "39794_winter_grid.tif",
             filetype = "GTiff",
@@ -359,12 +306,10 @@ writeVector(akde_result_winter_grid,
             overwrite=TRUE)
 
 
-###---------- Correct CRS and projection SUMMER 94----
+###############Summer 94
 
 
-
-
-# Subset the dates when in summer habitat
+#subset when in summer habitat
 summer_data_grid <- subset(
   moose_collars_gps,
   CollarID == "39794" & 
@@ -372,13 +317,13 @@ summer_data_grid <- subset(
        (LMT > "2019-04-16" & LMT < "2019-09-02"))
 )
 
-# Make as sf to apply coordinates attributes, transform, so units would be meters
+#make as sf to apply coordinates attributes, transform, so units would be meters
 summer_sf_grid <- st_transform(st_as_sf(summer_data_grid, coords = c("Lon", "Lat"), crs = 4326), crs = 32635) ## change of crs
 summer_coordinates_grid <- st_coordinates(summer_sf_grid)
 summer_sf_grid$Easting <- summer_coordinates_grid[, "X"]
 summer_sf_grid$Northing <- summer_coordinates_grid[, "Y"]
 
-# Reformat in the way that as.telemetry accepts
+#format in the way that as.telemetry accepts
 summer_df_grid <- data.frame(
   id = as.factor(summer_sf_grid$CollarID),
   UTM.Easting = summer_sf_grid$Easting,
@@ -387,10 +332,8 @@ summer_df_grid <- data.frame(
   timestamp = as.POSIXct(summer_sf_grid$LMT, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Tallinn")
 )
 
-# Clean the data
 summer_df_grid <- na.omit(summer_df_grid)
 
-# Get telemetry
 summer_tel_grid <- as.telemetry(summer_df_grid,
                            drop = FALSE,
                            crs = 32635,
@@ -417,19 +360,6 @@ writeVector(akde_result_summer_grid,
             overwrite=TRUE)
 
 
-#--- testing different models & evaluation methods ----
-tel_39793 <- tel[["39793"]] 
-
-guess <- ctmm.guess(tel_39793, interactive = FALSE)
-models <- ctmm.select(tel_39793, guess,verbose = TRUE)
-
-
-
-
-
-
-
-
 
 
 
@@ -438,7 +368,7 @@ models <- ctmm.select(tel_39793, guess,verbose = TRUE)
 
 
 # second period
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! This needs to be weighted later
+#!!!!!!!!!!!!!!!!!!!!!!!!! This needs to be weighted later
 #subset the dates when in winter habitat
 # first_data <- subset(
 #   moose_collars_gps,
